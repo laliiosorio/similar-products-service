edbccc041371bab0ecf1683d9ab893a0a2574d35
laliiosorio <laliiosorio@gmail.com>
2026-06-24 01:15:57 +0200

    cache mock responses

---
+++ src/constants/cache.js
@@ -0,0 +1,7 @@
+const productCache = new Map();
+const similarIdsCache = new Map();
+
+module.exports = {
+  productCache,
+  similarIdsCache,
+};
--- src/services/mock-product.service.js
+++ src/services/mock-product.service.js
@@ -1,10 +1,17 @@
 const httpClient = require('../config/http-client');
 const { mockApiUrl } = require('../config/env');
+const { productCache, similarIdsCache } = require('../constants/cache');
 const AppError = require('../errors/app-error');
 
 async function getSimilarIds(productId) {
+  if (similarIdsCache.has(productId)) {
+    console.log('CACHE HIT SIMILAR IDS', productId);
+    return similarIdsCache.get(productId);
+  }
+
   try {
     const response = await httpClient.get(`${mockApiUrl}/product/${productId}/similarids`);
+    similarIdsCache.set(productId, response.data);
     return response.data;
   } catch (error) {
     if (error.response?.status === 404) {
@@ -16,8 +23,14 @@ async function getSimilarIds(productId) {
 }
 
 async function getProductById(productId) {
+  if (productCache.has(productId)) {
+    console.log('CACHE HIT PRODUCT', productId);
+    return productCache.get(productId);
+  }
+
   try {
     const response = await httpClient.get(`${mockApiUrl}/product/${productId}`);
+    productCache.set(productId, response.data);
     return response.data;
   } catch (error) {
     if (error.response?.status === 404) {
