6b4d0502a9eeef8ee9a955cec8242a46776a5abf
laliiosorio <laliiosorio@gmail.com>
2026-06-23 23:52:26 +0200

    improve error handling

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -3,11 +3,11 @@ const mockProductService = require('./mock-product.service');
 async function getSimilarProducts(productId) {
   const similarIds = await mockProductService.getSimilarIds(productId);
 
-  const products = await Promise.all(
+  const results = await Promise.allSettled(
     similarIds.map((similarId) => mockProductService.getProductById(similarId)),
   );
 
-  return products;
+  return results.filter((result) => result.status === 'fulfilled').map((result) => result.value);
 }
 
 module.exports = {
